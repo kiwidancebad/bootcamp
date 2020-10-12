9273333d2743ce8a220064136fd0f581d18b4fc8
kiwidancebad <kiwidancebad@gmail.com>
2020-10-12 10:24:21 +0300

    add resovlers

--- spacex-explorer/server/src/index.js
+++ spacex-explorer/server/src/index.js
@@ -1,19 +1,42 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 const { createStore } = require('./utils/db');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
+const isEmail = require('isemail');
+
 const store = createStore();
+
+const dataSources = () => ({
+  launchAPI: new LaunchAPI(),
+  userAPI: new UserAPI({ store }),
+});
+
+const context = async ({ req }) => {
+  const auth = req.headers?.authorization || '';
+  const email = new Buffer(auth, 'base64').toString('ascii');
+
+  if (!isEmail.validate(email)) {
+    return { user: null };
+  }
+
+  const users = await store.users.findOrCreate({ where: { email }});
+  const user = users && users[0] ? users[0] : null;
+
+  return { user };
+}
 
 const server = new ApolloServer({
   typeDefs,
-  dataSources: () => ({
-    launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI({ store }),
-  }),
+  resolvers,
+  dataSources,
+  context,
+  playground: true,
+  introspection: true,
 });
 
 if (process.env.NODE_ENV !== 'test') {
---
+++ spacex-explorer/server/src/resolvers.js
@@ -0,0 +1,97 @@
+const { paginateResults } = require('./utils/requests');
+
+module.exports = {
+  Query: {
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunces();
+      allLaunches.reverse();
+
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches,
+      });
+
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        hasMore: launches.length ?
+          launches[launches.length - 1].cursor !==
+          allLaunches[allLaunches.length - 1].cursor
+          : false,
+      };
+    },
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: async (_, __, { dataSources }) =>
+      dataSources.userAPI.findOrCreateUser(),
+  },
+  Mutation: {
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const result = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+
+      return {
+        success: result?.length === launchIds.length,
+        message:
+          result.length === launch.length
+            ? 'trips booked successfully'
+            : `the following launches couldn't be booked: ${launchIds.filter(
+              id => !results.includes(id),
+            )}`,
+        launches,
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result) {
+        return {
+          success: false,
+          message: 'failed to cancel the trip',
+        };
+      }
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch],
+      };
+    },
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) {
+        user.token = new Buffer(email).toString('base64');
+        return user;
+      }
+    },
+    uploadProfileImage: async (_, { file }, { dataSources }) => 
+      dataSources.userAPI.uploadProfileImage({ file }),
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) => 
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => size === 'SMALL'
+      ? mission.missionPatchSmall
+      : mission.missionPatchLarge,
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) {
+        return [];
+      }
+
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      )
+    },
+  },
+}
